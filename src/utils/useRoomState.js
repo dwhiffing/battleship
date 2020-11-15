a77b40226886b343a8676011aed4fcd4d8d34bc5
Daniel Whiffing <danielwhiffing@gmail.com>
2020-11-15 18:06:39 -0500

    test

--- src/utils/useRoomState.js
+++ src/utils/useRoomState.js
@@ -7,12 +7,12 @@ export const useRoomState = ({ room, setRoom }) => {
     if (!room) return
     setServerState(room.state.toJSON())
     room.onStateChange((state) => {
-      if (!state.players.toJSON().some((p) => p.id === room.sessionId)) {
-        room.leave()
-        localStorage.removeItem(room.id)
-        setServerState({})
-        setRoom()
-      }
+      // if (!state.players.toJSON().some((p) => p.id === room.sessionId)) {
+      //   room.leave()
+      //   localStorage.removeItem(room.id)
+      //   setServerState({})
+      //   setRoom()
+      // }
       setServerState(state.toJSON())
     })
 
@@ -21,13 +21,13 @@ export const useRoomState = ({ room, setRoom }) => {
     // setTimeout(() => setMessage(''), 5000)
     // })
 
-    setTimeout(() => {
-      room.onLeave(() => {
-        localStorage.removeItem(room.id)
-        setServerState({})
-        setRoom()
-      })
-    }, 10000)
+    // setTimeout(() => {
+    //   room.onLeave(() => {
+    //     localStorage.removeItem(room.id)
+    //     setServerState({})
+    //     setRoom()
+    //   })
+    // }, 10000)
   }, [room, setRoom])
 
   return [serverState]
